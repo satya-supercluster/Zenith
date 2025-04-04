aeab9ee38c426affd85036d9af7f338e2cd41a0b
satya-supercluster <satya.supercluster@gmail.com>
2025-04-05 00:57:08 +0530

    Update login API endpoint to use production URL

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -21,8 +21,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      // const response = await fetch("https://zenith-club-manit.onrender.com/api/admin/login", {
-      const response = await fetch("http://localhost:3001/api/admin/login", {
+      const response = await fetch("https://zenith-club-manit.onrender.com/api/admin/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
